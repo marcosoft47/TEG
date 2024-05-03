f01ffb9a3aa7ba06c9dc3e5a2eb6116b6a082bad
Yuri-Jeik-Becker <ybecker00@hotmail.com>
2024-05-03 16:11:04 -0300

    update

--- trabalho01/functions.c
+++ trabalho01/functions.c
@@ -1,23 +1,34 @@
 
 
-for (int i = 0; i < quantLinhas; i++)
+for (int i = 0; i < nLinhas; i++)
 {
-    for (int a = i; a < quantLinhas; a++)
+    for (int a = i; a < nLinhas; a++)
     {
         if (a != i)
         {
-            for (int j = 0; j < quantColunas; j++)
+            for (int j = 0; j < nColunas; j++)
             {
                 aux0 = aux0 + (sqrt(pow(matrizEntrada[a][j], 2)) - sqrt(pow(matrizEntrada[i][j], 2)));
             }
-            if (aux0 > maiorDist){
+            if (aux0 > maiorDist)
+            {
                 maiorDist = aux0;
             }
+            matrizSaida[i][a] = aux0;
+            matrizSaida[a][i] = aux0;
             aux0 = 0;
         }
         else
         {
             matrizSaida[i][a] = 0;
         }
+    }
+}
+
+for (int i = 0; i < nLinhas; i++)
+{
+    for (int a = 0; a < nLinhas; a++)
+    {
+        matrizSaida[i][a] = matrizSaida[i][a] / maiorDist;
     }
 }
