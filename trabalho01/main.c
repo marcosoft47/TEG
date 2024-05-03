1607f0f51b2751f59b6099a5edce36e72e8f8888
marcosoft47 <56490702+marcosoft47@users.noreply.github.com>
2024-05-03 16:25:20 -0300

    lendo o csv

--- trabalho01/main.c
+++ trabalho01/main.c
@@ -1,31 +1,53 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#define nLinhas 150
+#define nColunas 4
 
-int manhattan(){
-}
-
+char *getField(const char* line, int num);
+int manhattan(float *v1, float *v2);
+void salvarCsv(FILE *fp, float matriz[nLinhas][nColunas]);
 
 int main(){
     // Ler arquivo
-    FILE *fpntr;
-    fpntr = fopen("IrisDataset.csv", "r");
+    FILE *fp;
+    fp = fopen("IrisDataset.csv", "r");
     
-    // Adicionar em matriz
-    int iris[150][4];
-    char linha[10];
-    fgets(linha, 10, fpntr);
+    float iris[nLinhas][nColunas];
+        int contador = 0;
+    char tmp[64], linha[nLinhas][64];
+
+    fgets(tmp, 64, fp);
+    while(fgets(tmp,64,fp))
+        strcpy(linha[contador++], tmp);
+    
+    for (int i = 0; i < nLinhas; i++){
+        for (int j = 0; j < nColunas; j++){
+            iris[i][j] = atof(getField(linha[i], j+1)); // +1 para ignorar a coluna de texto
+        }
+    }
+    fclose(fp);
+
 
     // Matriz distância Manhattan Normalizada
-
+    for (int i = 0; i < nLinhas; i++){
+        for (int j = 0; j < nColunas; j++){
+            printf("%f ", iris[i][j]);
+        }
+        printf("\n");
+    }
 
     // Matriz adjacencia (manhattan <= 0.3)
+    float mAdjacencia[nLinhas][nLinhas];
 
     // Salvar em .csv
 
     // Ver git caloio
 }
 
+// Pega o campo 
 char *getField(const char* line, int num) {
+    num++;
     const char *p = line;
     size_t len;
     char *res;
@@ -43,4 +65,26 @@ char *getField(const char* line, int num) {
         res[len] = '\0';
     }
     return res;
+}
+
+int manhattan(float *v1, float *v2){
+    // float resultado = 0;
+    // for(int i = 0; i <nColunas; i++)
+    //     resultado += sqrt(pow(v1[i], 2)) - sqrt(pow(v2[i], 2));
+    // return resultado;
+}
+
+void salvarCsv(FILE *fp, float matriz[nLinhas][nColunas]){
+    int contador = 0;
+    char tmp[64], linha[nLinhas][64];
+
+    fgets(tmp, 10, fp);
+        while(fgets(tmp,64,fp))
+        strcpy(linha[contador++], tmp);
+    
+    for (int i = 0; i < nLinhas; i++){
+        for (int j = 0; j < nColunas; j++){
+            matriz[i][j] = atof(getField(linha, j+1)); // +1 para ignorar a coluna de texto
+        }
+    }
 }
